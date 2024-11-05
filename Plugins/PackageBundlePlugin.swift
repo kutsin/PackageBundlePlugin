7dcbd35509410bd4c80fa15c82d3347ba61a41b6
mr.kutsin <mr.kutsin@gmail.com>
2024-11-05 01:06:39 -0600

    fix

--- Plugins/PackageBundlePlugin.swift
+++ Plugins/PackageBundlePlugin.swift
@@ -1,47 +1,47 @@
 import PackagePlugin
 import Foundation
 
-@main
-struct PackageBundlePlugin: BuildToolPlugin {
-    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-        guard target.sourceModule != nil else { return [] }
-
-        let output = context.pluginWorkDirectoryURL.appending(queryItems: [URLQueryItem(name: "PackageBundle.g.swift", value: nil)])
-        
-        print("+=+=+=+=+=+ \(output)")
-
-        return [
-            .buildCommand(
-                displayName: "Generating package bundle extension for \(target.name)",
-                executable: try context.tool(named: "PackageBundleGenerator").url,
-                arguments: [],
-                environment: [:],
-                inputFiles: [],
-                outputFiles: [output]
-            )
-        ]
-    }
-}
-
 //@main
 //struct PackageBundlePlugin: BuildToolPlugin {
 //    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
 //        guard target.sourceModule != nil else { return [] }
 //
-//        let output = context.pluginWorkDirectory.appending(subpath: "PackageBundle.g.swift")
+//        let output = context.pluginWorkDirectoryURL.appending(queryItems: [URLQueryItem(name: "PackageBundle.g.swift", value: nil)])
+//        
+//        print("+=+=+=+=+=+ \(output)")
 //
 //        return [
 //            .buildCommand(
 //                displayName: "Generating package bundle extension for \(target.name)",
-//                executable: try context.tool(named: "PackageBundleGenerator").path,
-//                arguments: [
-//                    context.package.displayName,
-//                    target.name,
-//                    output,
-//                ],
+//                executable: try context.tool(named: "PackageBundleGenerator").url,
+//                arguments: [],
+//                environment: [:],
 //                inputFiles: [],
 //                outputFiles: [output]
 //            )
 //        ]
 //    }
 //}
+
+@main
+struct PackageBundlePlugin: BuildToolPlugin {
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        guard target.sourceModule != nil else { return [] }
+
+        let output = context.pluginWorkDirectory.appending(subpath: "PackageBundle.g.swift")
+
+        return [
+            .buildCommand(
+                displayName: "Generating package bundle extension for \(target.name)",
+                executable: try context.tool(named: "PackageBundleGenerator").path,
+                arguments: [
+                    context.package.displayName,
+                    target.name,
+                    output,
+                ],
+                inputFiles: [],
+                outputFiles: [output]
+            )
+        ]
+    }
+}
